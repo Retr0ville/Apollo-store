807e727e1498ee9acf7264c1c554ff32037de4ee
Retr0ville <xclusivesam14@gmail.com>
2022-08-13 05:15:55 +0100

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React, { PureComponent } from 'react';
-import './App.css';
+import React, { PureComponent } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./layout/Header";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import ProductDetail from "./pages/ProductDetail";
+
 
 class App extends PureComponent {
   render() {
     return (
-      <div>
-      </div>
+      <>   
+        <Router>
+        <Header />
+          <Routes>
+            <Route path="/" element={<ProductList/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/details/:itemId" element={<ProductDetail/>} />
+          </Routes>
+        </Router>
+      </>
     );
   }
 }
